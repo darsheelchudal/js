bc71062801901ba4d844739eb99af8876e5a9b49
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-05-05 14:23:48 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553; //cannot be changed
+let accountEmail = "darsheelchudal11@gmail.com"; //
+var accountPassword = "2hjxb";
+accountCity = "Kathmandu";
+
+// accountId = 2; //not allowed
+
+accountEmail = "chudalkharudev@gmail.com";
+accountPassword = "16738";
+accountCity = "Jhapa";
+let accountState;
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+/*
+Prefer not to use var because of issue in block scope and functional scope
+*/
+
---
+++ 01_basics/02_variables.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3); we are using node js not browser
+
+console.log(3 + 3);
+console.log("Darsheel");
+
+let name = "Darsheel";
+let age = 21;
+let isLoggedIn = false;
+let state;
+
+
+//Number
+//BigInt 
+//String  '' or ""
+//Boolean TRUE OR FALSE
+//null => standalone value
+//undefined =>
+//symbol 
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
